70e459e6c2b05311780914003cc49c98a9d7f488
theoltc <theo.le-tohic@epitech.eu>
2025-04-30 10:23:01 -0400

    add

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-const userRoutes = require("../routes/userRoutes");
+const userRoutes = require("../routes/UserRoutes");
 const bikeRoutes = require("../routes/bikeRoutes");
 const authRoutes = require("../routes/authRoutes");
 
