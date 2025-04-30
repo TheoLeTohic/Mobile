186866002060accbe8fd1c8a8b6a33feafa0fc83
theoltc <theo.le-tohic@epitech.eu>
2025-04-30 09:55:04 -0400

    add

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const userRoutes = require('./routes/userRoutes');
-const bikeRoutes = require('./routes/bikeRoutes');
-const authRoutes = require('./routes/authRoutes');
+const userRoutes = require("./routes/userRoutes");
+const bikeRoutes = require("./routes/bikeRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
@@ -13,18 +13,23 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/bikes', bikeRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/bikes", bikeRoutes);
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 // Connect to MongoDB and start server
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
